862ad20558ed5d6c1eeb3c38a1db058de5004da8
Anwesh43 <anweshthecool0@gmail.com>
2020-04-01 00:36:45 +0530

    adding State class

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+const scGap = 0.02
+const delay = 30
+
+class State {
+    scale = 0
+    dir = 0
+    prevScale = 0
+
+    update(cb) {
+        this.scale += scGap * this.dir
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            cb(this.scale)
+        }
+    }
+
+    startUpdating(cb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            cb()
+        }
+    }
+}
+
+
 const vueInstance = new Vue({
     el : '#app',
     data: {
@@ -9,7 +36,9 @@ const vueInstance = new Vue({
             this.children++
         },
         sub() {
-            this.children--
+            if (this.children > 0) {
+                this.children--
+            }
         }
     }
 })
