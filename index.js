80cd6ed6a9dc476fa3ea837c461a3b9477b5bd79
Anwesh43 <anweshthecool0@gmail.com>
2020-03-31 23:55:42 +0530

    adding main vue instance

---
+++ index.js
@@ -0,0 +1,14 @@
+const vueInstance = new Vue({
+    data: {
+        return {children : 0}
+    },
+
+    methods: {
+        add() {
+            this.children++
+        },
+        sub() {
+            this.children--
+        }
+    }
+})
